54d0600da850bbf0689ac6d83b223cb6db37473c
Indira Rully Pricilia <92698567+Indirarully@users.noreply.github.com>
2021-11-19 00:41:23 +0700

    Update Latihan2.py

--- Latihan2.py
+++ Latihan2.py
@@ -1,10 +1,11 @@
+
 max=0
 while True:
    a=int(input('Masukkan bilangan='))
   
-  if max < a:
-    max = a
-  if a==0:
-break
+   if max < a:
+      max = a
+   if a==0:
+      break
 
 print('Bilangan terbesarnya adalah',max)
