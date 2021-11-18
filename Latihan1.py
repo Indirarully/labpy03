2d41e6fab272c370d7233bba14572639f675227f
Indira Rully Pricilia <92698567+Indirarully@users.noreply.github.com>
2021-11-19 00:39:53 +0700

    Update Latihan1.py

--- Latihan1.py
+++ Latihan1.py
@@ -1,13 +1,13 @@
 
-print ('Masukkan nilai N: 5')
-import random
-jumlah = 5
-   a = 0
+print("*******Latihanl*********)
 
-for x in range(jumlah):
-  i = random.uniform(.0,.5)
+print import
+("masukan random
+jumlah 5
+a = 0
+for x in range (jumlah) :
+    i = random.uniform(.0,.5)
     a+=1
+    print ('data ke :',a,'=>', 1)
 
-print('data ke:',a,'==>', i)
-
-print ('selesai')
+print (***** SELESAI *****)
